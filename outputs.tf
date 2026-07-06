6135bf4073c9a05f8ad690e828bb9bf1e1f47d5e
Daniel Allen <dan@aeternagroup.net>
2026-07-06 23:14:50 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -2,4 +2,20 @@ output "security_center_subscription_pricings" {
   description = "All security_center_subscription_pricing resources"
   value       = azurerm_security_center_subscription_pricing.security_center_subscription_pricings
 }
+output "security_center_subscription_pricings_extension" {
+  description = "List of extension values across all security_center_subscription_pricings"
+  value       = [for k, v in azurerm_security_center_subscription_pricing.security_center_subscription_pricings : v.extension]
+}
+output "security_center_subscription_pricings_resource_type" {
+  description = "List of resource_type values across all security_center_subscription_pricings"
+  value       = [for k, v in azurerm_security_center_subscription_pricing.security_center_subscription_pricings : v.resource_type]
+}
+output "security_center_subscription_pricings_subplan" {
+  description = "List of subplan values across all security_center_subscription_pricings"
+  value       = [for k, v in azurerm_security_center_subscription_pricing.security_center_subscription_pricings : v.subplan]
+}
+output "security_center_subscription_pricings_tier" {
+  description = "List of tier values across all security_center_subscription_pricings"
+  value       = [for k, v in azurerm_security_center_subscription_pricing.security_center_subscription_pricings : v.tier]
+}
 
