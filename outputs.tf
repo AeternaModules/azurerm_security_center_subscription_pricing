9e412be71e2fa1643d61a5fb8013e87057f8fb67
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:20:08 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,21 +1,17 @@
-output "security_center_subscription_pricings" {
-  description = "All security_center_subscription_pricing resources"
-  value       = azurerm_security_center_subscription_pricing.security_center_subscription_pricings
-}
 output "security_center_subscription_pricings_extension" {
-  description = "List of extension values across all security_center_subscription_pricings"
-  value       = [for k, v in azurerm_security_center_subscription_pricing.security_center_subscription_pricings : v.extension]
+  description = "Map of extension values across all security_center_subscription_pricings, keyed the same as var.security_center_subscription_pricings"
+  value       = { for k, v in azurerm_security_center_subscription_pricing.security_center_subscription_pricings : k => v.extension }
 }
 output "security_center_subscription_pricings_resource_type" {
-  description = "List of resource_type values across all security_center_subscription_pricings"
-  value       = [for k, v in azurerm_security_center_subscription_pricing.security_center_subscription_pricings : v.resource_type]
+  description = "Map of resource_type values across all security_center_subscription_pricings, keyed the same as var.security_center_subscription_pricings"
+  value       = { for k, v in azurerm_security_center_subscription_pricing.security_center_subscription_pricings : k => v.resource_type }
 }
 output "security_center_subscription_pricings_subplan" {
-  description = "List of subplan values across all security_center_subscription_pricings"
-  value       = [for k, v in azurerm_security_center_subscription_pricing.security_center_subscription_pricings : v.subplan]
+  description = "Map of subplan values across all security_center_subscription_pricings, keyed the same as var.security_center_subscription_pricings"
+  value       = { for k, v in azurerm_security_center_subscription_pricing.security_center_subscription_pricings : k => v.subplan }
 }
 output "security_center_subscription_pricings_tier" {
-  description = "List of tier values across all security_center_subscription_pricings"
-  value       = [for k, v in azurerm_security_center_subscription_pricing.security_center_subscription_pricings : v.tier]
+  description = "Map of tier values across all security_center_subscription_pricings, keyed the same as var.security_center_subscription_pricings"
+  value       = { for k, v in azurerm_security_center_subscription_pricing.security_center_subscription_pricings : k => v.tier }
 }
 
